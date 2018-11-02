ce29319e45909e2f8ac5d98a5b66c617a50ea61c
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-02 16:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ function Scooter(year, color, model) {
 //let honda = new Scooter(2015, 'red','swoosh')
 
 function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
 
 }
 

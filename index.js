ad3a88edcad8a43d0e71f2ce53dee14ee7cdb7a2
ToAnh Tran <toanh.t.tran@gmail.com>
2018-11-02 16:59:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function Scooter(year, color, model) {
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+
+//let honda = new Scooter(2015, 'red','swoosh')
+
+function Driver(name, age, experience) {
+  this.name = name
+  this.age = age
+  this.experience = experience
+
+}
+
+function PickupLocation(address, city) {
+  this.address = address
+  this.city = city
+}
